0c8b4d8daef52c6ff6f30f834149d857f168d53e
ddcat <ddcatena@gmail.com>
2023-09-11 18:06:21 -0400

    this godam database

--- database.py
+++ database.py
@@ -10,9 +10,15 @@ myDB = mysql.connector.connect(
 
 myCurser = myDB.cursor()
 
-#table1 = "CREATE TABLE Events (sport_nice VARCHAR(50) PRIMARY KEY NOT NULL, site VARCHAR(50) NOT NULL)"
+#USE THIS LATER
+"FOREIGN KEY(sport_name) REFERENCES Events(sport_nice)"
+#
+
+table1 = "CREATE TABLE EventsTest (id int PRIMARY KEY AUTO_INCREMENT, sport_nice VARCHAR(50) NOT NULL)"
 #myCurser.execute(table1)
-table2 = "CREATE TABLE SportsTablee (sport_name VARCHAR(50) PRIMARY KEY NOT NULL, commence_time VARCHAR(50) NOT NULL, odds_one int NOT NULL, odds_two int NOT NULL, teams1 VARCHAR(50) NOT NULL, teams2 VARCHAR(50), site VARCHAR(50) NOT NULL)"
+
+#NEED TO MAKE PRIMARY KEY THE ID OF THE MATCH
+table2 = "CREATE TABLE SportsTest2 (sport_id int PRIMARY KEY, FOREIGN KEY(sport_id) REFERENCES EventsTest(id), sport_name VARCHAR(50) NOT NULL, sport_id VARCHAR(50) NOT NULL, commence_time VARCHAR(50) NOT NULL, odds_one int NOT NULL, odds_two int NOT NULL, teams1 VARCHAR(50) NOT NULL, teams2 VARCHAR(50), site VARCHAR(50) NOT NULL)"
 #myCurser.execute(table2)
 
 class dataInsertion():
@@ -23,6 +29,14 @@ class dataInsertion():
     #Returns the Event of a given sport name
     def getEventTableForSport(sport_nice):
         return myCurser.execute("SELECT * FROM SportsTablee WHERE sport_nice = $sport_nice")
+    
+    def testInsertion(thing, eventsPlug, SportsPlug):
+        sata = "INSERT INTO EventsTest (sport_nice) VALUES(%s)"
+        #print(eventsPlug)
+        myCurser.executemany(sata, eventsPlug)
+
+        myCurser.executemany("INSERT INTO SportsTest2 (sport_name, sport_id, commence_time, odds_one, odds_two, teams1, teams2, site) VALUES(%s, %s,%s, %s, %s, %s, %s, %s)", SportsPlug)
+        myDB.commit()
     
     #adds given event to the Event Table
     def addToEventTable(id, sports):
@@ -35,11 +49,12 @@ class dataInsertion():
     
 
 test = dataInsertion()
+sporttist = [('ass', )]
+sportsTable = [("ass", "@#$^&*FGHJK", "1111111", -120, 120, "beng", "jags", "JERK")]
 
-sportsTable = [("ass", "1111111", -120, 120, "beng", "jags", "JERK")]
-
+#test.testInsertion(sporttist, sportsTable)
 #myCurser.execute("DESCRIBE SportsTable")
-myCurser.execute("SELECT * FROM SportsTablee")
+myCurser.execute("SELECT * FROM SportsTest2")
 for x in myCurser:
     print(x)
 #print(myCurser.fetchall())
--- testJson.py
+++ testJson.py
@@ -15,30 +15,32 @@ for dataSet in json_object['data']:
     elif(dataSet['sites'][0] == []):
         continue
     else:
+        #COLLECTING WANTED VALUES
         sport_name = dataSet['sport_nice']
         teams = dataSet['teams']
         odds = dataSet['sites'][0]['odds']['h2h']
         time = dataSet['commence_time']
         sites = dataSet['sites']
+        sport_id = dataSet['id']
 
         #Getting rid of soccer for now
         if(len(odds) >= 3):
             continue
 
-        data[tuple(teams)] = tuple([sport_name, odds, time, sites])
+        data[tuple(teams)] = tuple([sport_name, sport_id, odds, time, sites])
 
 
 dataInserter = dataInsertion()
-
+#PUTTING INFO INTO THE DATABASE
 for info in data.keys():
     temp = data[info] 
     #print((tuple(temp[0]), temp[2], temp[1][0], temp[1][1], info[0], info[1], "FanDuel"))
-
-    sportTable = [(temp[0], temp[2], temp[1][0], temp[1][1], info[0], info[1], "FanDuel")]
-    #dataInserter.addToEventTable(sportTable)
+    sportName = [(temp[0])]
+    sportTable = [(temp[0], temp[1], temp[3], temp[2][0], temp[2][1], info[0], info[1], "FanDuel")]
+    dataInserter.testInsertion(sportName, sportTable)
 
 
 print()
-(json_object['data'])
+#print(json_object['data'])
 print()
-print(data)
+#print(data)
